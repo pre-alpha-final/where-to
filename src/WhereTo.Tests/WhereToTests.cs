0f6201ee3322ede3ee504a53b6c40f3f33a07314
spanishprisoner <jacek.mowinski@gmail.com>
2019-02-12 17:59:22 +0100

    Running test project

---
+++ src/WhereTo.Tests/WhereToTests.cs
@@ -0,0 +1,16 @@
+﻿using WhereTo.Parser;
+using Xunit;
+
+namespace WhereTo.Tests
+{
+	public class WhereToTests
+	{
+		[Theory]
+		[InlineData("a=1.1 or b=1.1")]
+		public void ShouldValidateCorrectInputs(string input)
+		{
+			var whereToParser = new WhereToParser();
+			whereToParser.Parse(input);
+		}
+	}
+}
